66e9c6be381fdf420684c52f285b7e5940983f05
akhilapidikiti <akhila.pidikiti9@gmail.com>
2020-04-22 09:50:06 +0530

    verifying users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,10 @@ class ApplicationController < ActionController::Base
     return @current_user if @current_user
 
     current_user_id = session[:current_user_id]
-    if current_user_id
+    if current_user_id and User.exists?(id = current_user_id)
       @current_user = User.find(current_user_id)
     else
+      session[:current_user_id] = nil
       nil
     end
   end
